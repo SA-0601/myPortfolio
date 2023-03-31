d2cd675c0f2f667a6ca37863663cbaf41e43e841
shubha agrawal <shubh.cap@gmail.com>
2023-03-31 10:10:17 -0400

    env file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,24 @@
-
+import Carousel from "react-bootstrap/Carousel";
 function Home() {
-  
-
-  
-  
   return (
     <div>
-      <h1 className="heading-primary">Hey, I'm <br></br>Shubha Agrawal</h1>
-      <p>lorem</p>
+      <h2 className="heading-secondary">Home</h2>
+
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="project-carousel "
+            src="https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHBvcnRmb2xpb3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h1 className="heading-primary">
+              Hey, I'm <br></br>Shubha Agrawal
+            </h1>
+            <p>Front End Developer</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
--- src/services/technewsapi.js
+++ src/services/technewsapi.js
@@ -1,9 +1,12 @@
+    //if you want to hide your api key
+    const apiKey = process.env.REACT_APP_API_KEY;
+
 //fetch data from API
 export const getTechNews = async () => {
     //make fetch request and store response
     try {
       const response = await fetch(
-        `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=4ef55ff38f87455690129bb8d178a84c`
+        `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${apiKey}`
       );
       const data = await response.json();
         return data;
